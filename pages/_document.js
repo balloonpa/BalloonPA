dadab1f01994b828f8c8ef57be07fb48d6a1b89c
BalloonPA <balloonpa.lp@gmail.com>
2025-11-16 00:26:44 +0700

    Update title

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>BallonPA</title>
+        <title>BalloonPA</title>
       </Head>
       <body className="antialiased">
         <Main />
