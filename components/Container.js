5459eed7ea3694047842f739d084e4bd4633b0d9
BalloonPA <balloonpa.lp@gmail.com>
2026-01-20 21:33:47 +0700

    deploy

--- components/Container.js
+++ components/Container.js
@@ -3,4 +3,4 @@ function Container({ children, className = "" }) {
   return <div className={`max-w-6xl mx-auto px-4 ${className}`}>{children}</div>;
 }
 
-export default Container;
+export default Container; 
